72a8df33d08407fa869fd7fd6b75d3908438e4b5
Valeria Sizikova <valery.sizikova@gmail.com>
2017-04-22 12:41:27 +0200

    added uuid generation

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,7 @@ import {Promise} from 'es6-promise';
 import {QueryAppModel} from "./QueryAppModel";
 import QueryItem from "./QueryItem";
 import Editor from "./Editor";
+import {Utils} from "./utils";
 
 export class AppContainer extends React.Component<IAppContainerProps, IAppContainerState> {
 
@@ -23,9 +24,9 @@ export class AppContainer extends React.Component<IAppContainerProps, IAppContai
 
     public componentDidMount() {
         var that = this;
+
         axios.get(`http://test.semmweb.com/sparql-cabinet/api/sparql/queries?api_key=c45480aa-f4a5-4224-a9fe-a8ecc2353a64`)
             .then(function (response) {
-                console.log(response);
                 that.setState({queries: response.data, selectedQuery: '0'})
             })
             .catch(function (error) {
@@ -33,25 +34,30 @@ export class AppContainer extends React.Component<IAppContainerProps, IAppContai
             })
     }
 
-    public selectQuery(queryId: string) {
+    public selectQuery(queryId:string) {
         this.props.model.select(queryId);
         this.setState({queries: this.state.queries, selectedQuery: queryId})
     }
 
     public render() {
 
-        var selectedQuery = parseInt(this.state.selectedQuery);
+        var that = this;
 
         if (this.state.queries.length > 0) {
             return <div>
                 <div className="col-xs-4">
                     <p>{this.props.model.name}</p>
                     {this.state.queries.map(query => {
-                        return <QueryItem key={query.id} query={query} onSelect={this.selectQuery.bind(this, query.id)} />
-                    })}
+                        return <QueryItem key={query.id} query={query}
+                                          onSelect={this.selectQuery.bind(this, query.id)}/>
+                        })}
                 </div>
                 <div className="col-xs-5">
-                    <Editor queryText={this.state.queries[selectedQuery].query} descriptionText={this.state.queries[selectedQuery].description}/>
+                    {this.state.queries.map(query => {
+                        if (query.id == that.state.selectedQuery) {
+                            return <Editor key={query.id} queryText={query.query} descriptionText={query.description}/>
+                            }
+                        })}
                 </div>
             </div>
         } else {
---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+/// <reference path="../typings/modules/react/index.d.ts" />
+/// <reference path="./interfaces.d.ts"/>
+
+export class Utils {
+
+    public static uuid():string {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+}
